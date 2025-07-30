8287544cba76b3cd87885ee6c0823adda5eed0ca
Shindhu@Zenith <86401655+Shindhu-Ramaswamy@users.noreply.github.com>
2025-07-30 19:28:24 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -4,15 +4,11 @@ public class Student {
     private int id;
     private String name;
     private String department;
-
-    // Constructor
     public Student(int id, String name, String department) {
         this.id = id;
         this.name = name;
         this.department = department;
     }
-
-    // Getters & Setters
     public int getId() { return id; }
     public String getName() { return name; }
     public String getDepartment() { return department; }
